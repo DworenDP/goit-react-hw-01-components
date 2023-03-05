f5481d2bca1d9ce1de5a8ad8972bc125fa193283
DworenDP <cuda.jaro@gmail.com>
2023-03-05 15:22:24 +0200

    fix class

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,15 +2,15 @@ import data from './data.json';
 
 export const Statistics = () => {
   return data.map(stats => {
-    console.log('stats', stats)
+    console.log('stats', stats);
     return (
-      <section class="statistics">
-        <h2 class="title">Upload stats</h2>
+      <section className="statistics">
+        <h2 className="title">Upload stats</h2>
 
-        <ul class="stat-list">
-          <li class="item" key={stats.id}>
-            <span class="label">{stats.label} </span>
-            <span class="percentage">{stats.percentage}</span>
+        <ul className="stat-list">
+          <li className="item" key={stats.id}>
+            <span className="label">{stats.label} </span>
+            <span className="percentage">{stats.percentage}</span>
           </li>
           {/* <li class="item" key={stats.id}>
             <span class="label">{stats.label} </span>
